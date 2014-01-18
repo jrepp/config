cb07d82e2d5bdadba573165688cc44e916a53d9d
root <root@li620-37.members.linode.com>
2014-01-18 18:40:54 +0000

    added additional packages for gnu-social

--- centos-install.sh
+++ centos-install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-yum install nginx php-common php-fpm php-mysqlnd-y
+yum install nginx php-common php-fpm php-mysqlnd-y php-mysql
+
+# status.net
+yum install php-gd php-xmlwriter php-mbstring php-dom
